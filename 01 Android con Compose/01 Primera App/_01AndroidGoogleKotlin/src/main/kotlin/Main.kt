a186a16574de9599cb81b5c8c94cbcb83e0afc0c
ivanmolero <imolerod@gmail.com>
2023-07-22 15:25:19 -0500

    finalizado kotlin variables

--- 01 Android con Compose/01 Primera App/_01AndroidGoogleKotlin/src/main/kotlin/Main.kt
+++ 01 Android con Compose/01 Primera App/_01AndroidGoogleKotlin/src/main/kotlin/Main.kt
@@ -1,3 +1,62 @@
+/*
+ * este es un comentario muy largo
+ * que toma mas de una linea en el
+ * texto
+ */
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    
+
+//    // Create a variable for the number of unread messages.
+//    var count = 10
+//    println("You have $count unread messages.")
+//
+//    // Decrease the number of messages by 1.
+//    count--
+//    println("You have $count unread messages.")
+
+//    val notificationEnabled = false
+//    println("Are notifications enabled? $notificationEnabled")
+
+//    println("Say \"hello\"")
+
+//    val nextMeeting = "Next meeting is: "
+//    val date = "January 1"
+//    val reminder = nextMeeting + date + " at work"
+//    println(reminder)
+
+//    val trip1: Double = 3.2
+//    val trip2: Double = 4.1
+//    val trip3: Double = 1.72
+//    val totalTripLength: Double = trip1 + trip2 + trip3
+//    println("$totalTripLength miles left to destination")
+
+//    var count = 10
+//    println("You have $count unread messages.")
+//    count++
+//    println("You have $count unread messages.")
+//    count--
+//    count--
+//    println("You have $count unread messages.")
+
+//    var cartTotal = 0
+//    println("Total: $cartTotal")
+//    cartTotal = 20
+//    println("Total: $cartTotal")
+
+//    val photosDeleted = 10
+//    val photosLeft = 90
+//    println("${photosLeft + photosDeleted} photos")
+//    println("$photosDeleted photos deleted")
+//    println("$photosLeft photos left")
+
+//    val unreadCount = 5
+//    val readCount = 100
+//    println("You have ${unreadCount + readCount} total messages in your inbox.")
+
+//    println("Hello Android!")
+//    println("Hello Ivan!")
+//    println(5 + 8)
+//    println("45" + "72")
+
 }
