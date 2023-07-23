43efe3615a74bc6919e6d298258000e06f195ffc
ivanmolero <imolerod@gmail.com>
2023-07-22 23:52:33 -0500

    finalizado agregar imagenes a la app

--- 01 Android con Compose/01 Primera App/A03ComposeBasicDesign/app/src/main/java/com/imolerodev/a03composebasicdesign/MainActivity.kt
+++ 01 Android con Compose/01 Primera App/A03ComposeBasicDesign/app/src/main/java/com/imolerodev/a03composebasicdesign/MainActivity.kt
@@ -3,13 +3,26 @@ package com.imolerodev.a03composebasicdesign
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.imolerodev.a03composebasicdesign.ui.theme.A03ComposeBasicDesignTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BirthdayGreetingWithImage(
+                        message = stringResource(id = R.string.happy_birthday_text),
+                        from = stringResource(id = R.string.from_text)
+                    )
                 }
             }
         }
@@ -30,17 +46,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(
+            message = message,
+            from = from
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column() {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.CenterHorizontally)
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp
+                )
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.End)
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp
+                )
+        )
+    }
+}
+
+@Preview(name = "Preview", showSystemUi = true)
+@Composable
+fun BirthdayGreetingWithTextPreview() {
     A03ComposeBasicDesignTheme {
-        Greeting("Android")
+        BirthdayGreetingWithImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.from_text)
+        )
     }
 }
